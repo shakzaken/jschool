b969dace35e457c945cf57f597d709dc68e88110
yakir zaken <yzaken@tremorvideo.com>
2021-02-09 14:50:23 +0200

    Adding jschool Java Spring backend.

---
+++ jschool-spring/src/main/java/com/shak/jschool/JschoolApplication.java
@@ -0,0 +1,13 @@
+package com.shak.jschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JschoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JschoolApplication.class, args);
+	}
+
+}
---
+++ jschool-spring/src/test/java/com/shak/jschool/JschoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shak.jschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JschoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
